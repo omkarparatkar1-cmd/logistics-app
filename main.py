013f7af13be88370f5cd87aead7fa8ea744526a8
omkarparatkar1-cmd <omkarparatkar1@gmail.com>
2026-02-07 18:59:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def main():
     logging.info("Application started")
     time.sleep(5)
     logging.info("Application finished successfully")
-
+print("Trigger test")
 
 if __name__ == "__main__":
+
     main()

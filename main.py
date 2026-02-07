5579879898963469b7fd00da93643b8e76205f89
omkarparatkar1-cmd <omkarparatkar1@gmail.com>
2026-02-07 18:39:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+﻿import logging
+import time
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    logging.info("Application started")
+    time.sleep(5)
+    logging.info("Application finished successfully")
+
+
+if __name__ == "__main__":
+    main()

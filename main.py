cc2246d05f1314a9fa4851ef55dcbe10843f21b1
omkarparatkar1-cmd <omkarparatkar1@gmail.com>
2026-02-07 19:15:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ logging.basicConfig(level=logging.INFO)
 def main():
     logging.info("Application started")
     time.sleep(5)
-    logging.info("Application finished successfully")
+    logging.info("Application   finished successfully")
 print("Trigger test")
 print("Trigger test 2")
 
 if __name__ == "__main__":
 
     main()
+
 
